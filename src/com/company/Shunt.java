e41195b5b2fd0cbdf8cdd51cc2f3527bb8e44f52
Linda MUCASSI <lmucassi@e4r10p2.wethinkcode.co.za>
2018-02-18 12:27:21 +0200

    initial commit

---
+++ src/com/company/Shunt.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Shunt {
+    public int que, st, ch, stLen;
+    public static int FILE_ERR = 1;
+    public static int FOMT_ERR = 2;
+    public static int USAGE_ERR = 3;
+
+}
